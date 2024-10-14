9a01fb57db0e04878b8a850ea4e28db48fdacae4
yulialener <lener.yu@yandex.ru>
2024-10-14 15:26:57 +0700

    Initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTest.java
@@ -0,0 +1,43 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@RunWith(AllureAndroidJUnit4.class)
+
+public class EspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    String text = "This is home fragment";
+
+    @Test
+    public void testName() {
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+        mainText.check(
+                matches(
+                        withText(text)
+                )
+        );
+    }
+}
+
+
+
+
